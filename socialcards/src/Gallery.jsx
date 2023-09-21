1e538e96e7311f988bb8ab83f847c54d8306e4c9
duffing09 <duffing18@gmail.com>
2023-09-20 20:00:08 -0400

    Added broken buttons to gallery

--- socialcards/src/Gallery.jsx
+++ socialcards/src/Gallery.jsx
@@ -1,5 +1,12 @@
+import React from "react";
+
 const Gallery = () => {
-  return <div>Gallery Page</div>;
-};
+  return (
+    <div>
+      <button>Broken Friends</button><button>Broken Everyone</button>
+      {/* <div>Greeting card</div> */}
+    </div>
+  )
+}
 
 export default Gallery;
