449b81b4b6d65dd04f0e04c3b323f1ad1cb3658b
Chris Riddle <stopimmediatley@gmail.com>
2023-09-21 14:17:07 -0400

    Add inside card functionality matching cover functionality

--- socialcards/src/CardMaker.jsx
+++ socialcards/src/CardMaker.jsx
@@ -8,7 +8,12 @@ const CardMaker = () => {
   const [coverText, setCoverText] = useState("");
   const [textOrientation, setTextOrientation] = useState("center");
   const [selectedFont, setSelectedFont] = useState("Arial");
+  const [insideBackgroundColor, setInsideBackgroundColor] = useState("#ffffff");
+  const [insideText, setInsideText] = useState("");
+  const [insideTextOrientation, setInsideTextOrientation] = useState("center");
+  const [insideSelectedFont, setInsideSelectedFont] = useState("Arial");
 
+  // cover-preview handlers
   const handleCoverBackgroundColorChange = (event) => {
     setCoverBackgroundColor(event.target.value);
   };
@@ -23,6 +28,23 @@ const CardMaker = () => {
 
   const handleFontChange = (event) => {
     setSelectedFont(event.target.value);
+  };
+
+  // inside-preview handlers
+  const handleInsideBackgroundColorChange = (event) => {
+    setInsideBackgroundColor(event.target.value);
+  };
+
+  const handleInsideTextChange = (event) => {
+    setInsideText(event.target.value);
+  };
+
+  const handleInsideTextOrientationChange = (event) => {
+    setInsideTextOrientation(event.target.value);
+  };
+
+  const handleInsideFontChange = (event) => {
+    setInsideSelectedFont(event.target.value);
   };
 
   return (
@@ -87,6 +109,64 @@ const CardMaker = () => {
             </select>
           </div>
           {/* COPY POINT */}
+          <div className='single-option-container'>
+            <label htmlFor='insideBackgroundColor'>
+              Choose inside background color:
+            </label>
+            <select
+              id='insideBackgroundColor'
+              name='insideBackgroundColor'
+              value={insideBackgroundColor}
+              onChange={handleInsideBackgroundColorChange}
+            >
+              <option value='#ffffff'>White</option>
+              <option value='#ff0000'>Red</option>
+              <option value='#00ff00'>Green</option>
+              <option value='#0000ff'>Blue</option>
+            </select>
+          </div>
+
+          <div className='single-option-container'>
+            <label htmlFor='insideText'>Add inside text here: </label>
+            <input
+              type='text'
+              id='insideText'
+              name='insideText'
+              value={insideText}
+              onChange={handleInsideTextChange}
+            />
+          </div>
+
+          <div className='single-option-container'>
+            <label htmlFor='insideTextOrientation'>
+              Choose inside text orientation:{" "}
+            </label>
+            <select
+              id='insideTextOrientation'
+              name='insideTextOrientation'
+              value={insideTextOrientation}
+              onChange={handleInsideTextOrientationChange}
+            >
+              <option value='left'>Left</option>
+              <option value='center'>Center</option>
+              <option value='right'>Right</option>
+            </select>
+          </div>
+
+          <div className='single-option-container'>
+            <label htmlFor='insideSelectedFont'>Choose inside font: </label>
+            <select
+              id='insideSelectedFont'
+              name='insideSelectedFont'
+              value={insideSelectedFont}
+              onChange={handleInsideFontChange}
+            >
+              <option value='Arial'>Arial</option>
+              <option value='Times New Roman'>Times New Roman</option>
+              <option value='Verdana'>Verdana</option>
+              <option value='Courier New'>Courier New</option>
+            </select>
+          </div>
         </div>
 
         <div className='preview-container'>
@@ -103,9 +183,14 @@ const CardMaker = () => {
           </div>
           <div
             className='inside-preview'
-            style={{ backgroundColor: coverBackgroundColor }}
+            style={{
+              backgroundColor: insideBackgroundColor,
+              textAlign: insideTextOrientation,
+              fontFamily: insideSelectedFont,
+            }}
           >
             <h2>Box for previewing the inside of the card.</h2>
+            <h2>{insideText}</h2>
           </div>
         </div>
       </div>
