4103f1105aebd57a01caf4602f0508186c5dc440
Jaylan Wilkins <139093449+WilkinsJay@users.noreply.github.com>
2023-09-24 21:38:28 -0400

    start of ShowCard component (#7)
    * strt of ShowCard component
    
    * WIP, ShowCard

--- socialcards/src/Gallery.jsx
+++ socialcards/src/Gallery.jsx
@@ -5,44 +5,44 @@ import Tilt from 'react-parallax-tilt';
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Gallery = ({ token }) => {
-  const [results, setResults] = useState([])
-  const [currentIndex, SetCurrentIndex] = useState(0);
+    const [results, setResults] = useState([])
+    const [currentIndex, SetCurrentIndex] = useState(0);
 
 
-  useEffect(() => {
-    axios
-      .get("https://social-cards.fly.dev/api/cards/", {
-        headers: {Authorization: `Token ${token}`},
-      })
-      .then((response) => {
-        setResults(response.data.results)
-      },
-      )
-  },[]
+    useEffect(() => {
+        axios
+            .get("https://social-cards.fly.dev/api/cards/", {
+                headers: { Authorization: `Token ${token}` },
+            })
+            .then((response) => {
+                setResults(response.data.results)
+            },
+            )
+    }, []
 
-  )
-  return (
-    <div>
-      <div className="galleryBox">
-        {results.map((card) => (
-          <Tilt>
-            <div className="galleryCard" key={results[currentIndex].id}
-            style={{backgroundColor: results[currentIndex].background_color}}>
-              {results[currentIndex].front_text}
+    )
+    return (
+        <div>
+            <div className="galleryBox">
+                {results.map((card) => (
+                    <Tilt>
+                        <div className="galleryCard" key={results[currentIndex].id}
+                            style={{ backgroundColor: results[currentIndex].background_color }}>
+                            {results[currentIndex].front_text}
+                        </div>
+                    </Tilt>
+
+                )
+
+                )}
             </div>
-          </Tilt>
-          
-        )
-
-        )}
-      </div>
-      <button onClick={() => currentIndex > 0 && SetCurrentIndex(currentIndex -1)}>Previous</button>
-      <button onClick={() => currentIndex < results.length -1 && SetCurrentIndex(currentIndex + 1)}>Next</button>
-      <button>Broken Friends</button><button>Broken Everyone</button>
-      <button>Log out</button>
-      {/* <div>Greeting card</div> */}
-    </div>
-  )
+            <button onClick={() => currentIndex > 0 && SetCurrentIndex(currentIndex - 1)}>Previous</button>
+            <button onClick={() => currentIndex < results.length - 1 && SetCurrentIndex(currentIndex + 1)}>Next</button>
+            <button>Broken Friends</button><button>Broken Everyone</button>
+            <button>Log out</button>
+            {/* <div>Greeting card</div> */}
+        </div>
+    )
 }
 
 export default Gallery;
